83c08faddc7dfa4ad88859d9ab7f98802c284d23
KAWALJEET SINGH <37301964+kawaljee@users.noreply.github.com>
2019-08-26 05:20:26 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+import Button from './components/Button'
 
-function App() {
-  return (
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      current : '',
+      previous : []
+    }
+  }
+render()
+  { 
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="result" type="text"  value={this.state.current} />
     </div>
   );
 }
+}
+
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react';
+
+
+class Button extends Component{
+    render(){
+        return (
+            <div>
+                <buttom class="calc-button">
+                    1
+                </buttom>
+            </div>
+        )
+    }
+}
+
+export default Button;
